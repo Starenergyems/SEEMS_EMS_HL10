5aa719b6cce9c0b943c8d8918aad73fec69d3d41
Peter <yoyo5022@gmail.com>
2023-11-30 14:50:45 +0800

    feat: linebot first implemt

--- app.js
+++ app.js
@@ -5,6 +5,43 @@ const Schema = require("./models/lc_schema");
 const methodOverride = require("method-override");
 const path = require("path");
 const port = 3000;
+const line = require('@line/bot-sdk');
+require('dotenv').config();
+
+app.post('/webhook', line.middleware({channelAccessToken:process.env.channelAccessToken, channelSecret:process.env.channelSecret}), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken:process.env.channelAccessToken,
+});
+
+function handleEvent(event) {
+  if (event.type === 'message' || event.message.type === 'text') {
+    return Promise.resolve(null);
+  }
+
+  if (event.type === 'follow') {
+    const userId = event.source.userId;
+    console.log(`User with ID ${userId} followed the bot`);
+    //add db update code here...
+    return client.replyMessage({
+      replyToken: event.replyToken,
+      messages: [{
+        type: 'text',
+        text: 'Thank you for following! Welcome to our bot!'
+      }],
+    });
+  }  
+}
+
+client.pushMessage({
+  to: userId,
+  messages: [{ type: 'text', text: 'hello, world' }]
+});
 
 mongoose
   .connect("mongodb://localhost:27017/ems")
