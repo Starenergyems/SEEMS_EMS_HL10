3c62b4573f561e8b8da87b1a4868486a03fec114
hugo <zixuanhugo@gmail.com>
2023-12-19 16:40:24 +0800

    feat : 新增可以在終端機查看現在連線路由

--- router/app.js
+++ router/app.js
@@ -4,26 +4,20 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
 const port = 3000;
-const router = express.Router();
 const app = express();
 const cors = require("cors");
 
 app.set("view engine", "ejs");
-// 設定視圖目錄為 C:\Test\SEEMS_EMS\views
 app.set("views", path.join(__dirname, "../views"));
-//use
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use("/public", express.static(path.join(__dirname, "../public")));
-//app.use(myMiddleware);
 app.use(cors());
 
 mongoose
   .connect("mongodb://localhost:27017/ems")
   .then(() => {
     console.log("成功連結mongoDB....");
-
-    // 檢查當前數據庫名稱
     const currentDBName = mongoose.connection.name;
     console.log("目前連線資料庫名稱：", currentDBName);
   })
@@ -32,26 +26,22 @@ mongoose
   });
 
 // 引入多個路由檔案
-//const router1 = require("./function"); //fun
-//const router2 = require("./middleware"); //處理驗證
+const router3 = require("./rAccount");
+const router4 = require("./rMode");
+const router5 = require("./rMeter");
+const router6 = require("./rPCS");
+const router7 = require("./rBattery");
+const router8 = require("./rCommu");
+const router9 = require("./rDevice");
+const router10 = require("./rEnvironment");
+const router11 = require("./rAlarm");
+const router12 = require("./rEvent");
+const router13 = require("./rReport");
+const router14 = require("./rChart");
+const { router, server } = require("./test");
 
-//畫面路由
-const router3 = require("./rAccount"); //帳號
-const router4 = require("./rMode"); //模式
-const router5 = require("./rMeter"); //運轉-電表 V
-const router6 = require("./rPCS"); //運轉-PCS V
-const router7 = require("./rBattery"); //運轉-BMS V
-const router8 = require("./rCommu"); //系統-通訊架構
-const router9 = require("./rDevice"); //系統-設備狀態
-const router10 = require("./rEnvironment"); //系統-環境監控
-const router11 = require("./rAlarm"); //告警
-const router12 = require("./rEvent"); //事件
-const router13 = require("./rReport"); //報表
-const router14 = require("./rChart"); //圖表
-
+app.use(router);
 // 使用這些路由
-//app.use(router1);
-//app.use(router2);
 app.use(router3);
 app.use(router4);
 app.use(router5);
@@ -64,28 +54,32 @@ app.use(router11);
 app.use(router12);
 app.use(router13);
 app.use(router14);
+//app.use(router15);
 
-//
-app.get("/", (req, res) => {
-  // num與fun
-  res.render("Login"); //渲染 Login.ejs
+// 額外的路由或中間件可以在這裡添加
+
+//終端機查看現在連線路由
+app.use((req, res, next) => {
+  console.log(
+    `Current API URL: ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
 });
 
-//* ~~~~~~~!!!!!!!!@@@@@@@@@@##########$$$$$$$$$$$$%%%%%%%%%^^^^^^^^^^^^^^&&&&&&&&&&&*********(((((((())))))))
+app.get("/", (req, res) => {
+  res.render("Login");
+});
 
 app.get("/login", (req, res) => {
-  // num與fun
-  res.render("Login"); //渲染 Login.ejs
+  res.render("Login");
 });
 
-// ~~~~~~~!!!!!!!!@@@@@@@@@@##########$$$$$$$$$$$$%%%%%%%%%^^^^^^^^^^^^^^&&&&&&&&&&&*********(((((((()))))))) */
-
 app.get("/error", (req, res) => {
-  // num與fun
   res.render("error");
 });
 
-// 監聽指定的端口
 app.listen(port, () => {
   console.log(`應用程式正在監聽端口 ${port}`);
 });
+
+app.listen();
