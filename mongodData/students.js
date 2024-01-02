0a8f2fc34ca308fb27787138b1810ec236a0434c
hugo <zixuanhugo@gmail.com>
2024-01-02 15:39:48 +0800

    chore: 增加MongoDB的測試資料

---
+++ mongodData/students.js
@@ -0,0 +1,13 @@
+{
+  "_id": {
+    "$oid": "6551c4b158888e1a848a4a06"
+  },
+  "name": "Hugo",
+  "age": 25,
+  "major": "Computer",
+  "scholarship": {
+    "merit": 6000,
+    "other": 7000
+  },
+  "__v": 0
+}
