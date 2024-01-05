6d80698ece5affa7fab7f6a308c9197042f792a4
Yuching <s.561318@gmail.com>
2024-01-05 16:54:58 +0800

    test: 開發報表/動態下拉式選單完成

--- public/scripts/Rpt_Report.js
+++ public/scripts/Rpt_Report.js
@@ -1,69 +1,5 @@
-// var myHeading = document.querySelector("h1");
-// myHeading.textContent = "Hello world!";
-
-
-const filtMonthROpts = document.querySelector(".filtMonthR .filtOptions");
-const filtSeasonROpts = document.querySelector(".filtSeasonR .filtOptions");
-const filtYearROpts = document.querySelector(".filtYearR .filtOptions");
-
-const dDL_filtMonR = document.querySelector(".title #dDL_filtMonthR");
-dDL_filtMonR.addEventListener("click", showHide_filtMonROpts);
-function showHide_filtMonROpts() {
-    filtMonthROpts.classList.toggle("appear");
-    filtSeasonROpts.classList.remove("appear");
-    filtYearROpts.classList.remove("appear");
-}
-
-const dDL_filtSeaR = document.querySelector(".title #dDL_filtSeasonR");
-dDL_filtSeaR.addEventListener("click", showHide_filtSeaROpts);
-function showHide_filtSeaROpts() {
-    filtSeasonROpts.classList.toggle("appear");
-    filtMonthROpts.classList.remove("appear");
-    filtYearROpts.classList.remove("appear");
-}
-
-const dDL_filtYeaR = document.querySelector(".title #dDL_filtYearR");
-dDL_filtYeaR.addEventListener("click", showHide_filtYeaROpts);
-function showHide_filtYeaROpts() {
-    filtYearROpts.classList.toggle("appear");
-    filtMonthROpts.classList.remove("appear");
-    filtSeasonROpts.classList.remove("appear");
-}
-
-const filterMonthR = document.querySelector(".title .filtMonthR p");
-const filterSeasonR = document.querySelector(".title .filtSeasonR p");
-const filterYearR = document.querySelector(".title .filtYearR p");
-
-document.addEventListener("click", hideFiltOptions);
-function hideFiltOptions(clickItem) {
-    if ((clickItem.target.id !== "dDL_filtMonthR") && (clickItem.target.id !== "dDL_filtSeasonR") && (clickItem.target.id !== "dDL_filtYearR")) {
-        if (clickItem.target.id === "filtNone") {
-            filterMonthR.textContent = "月報";
-            filterSeasonR.textContent = "季報";
-            filterYearR.textContent = "年報";
-        } else if ((clickItem.target.id === "monthRFO_01") || (clickItem.target.id === "monthRFO_02") || (clickItem.target.id === "monthRFO_03")) {
-            filterMonthR.textContent = clickItem.target.textContent;
-            filterSeasonR.textContent = "季報";
-            filterYearR.textContent = "年報";
-        } else if ((clickItem.target.id === "seasonRFO_01") || (clickItem.target.id === "seasonRFO_02") || (clickItem.target.id === "seasonRFO_03")) {
-            filterSeasonR.textContent = clickItem.target.textContent;
-            filterMonthR.textContent = "月報";
-            filterYearR.textContent = "年報";
-        } else if ((clickItem.target.id === "yearRFO_01") || (clickItem.target.id === "yearRFO_02")) {
-            filterYearR.textContent = clickItem.target.textContent;
-            filterMonthR.textContent = "月報";
-            filterSeasonR.textContent = "季報";
-        }
-
-        filtMonthROpts.classList.remove("appear");
-        filtSeasonROpts.classList.remove("appear");
-        filtYearROpts.classList.remove("appear");
-    }
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
 $(document).ready(function () {
+    generateYearOptions();
 
     let lang = {
         sProcessing: "處理中...",
@@ -92,100 +28,56 @@ $(document).ready(function () {
     };
     var dataset = [{
         "index": "1",
-        "reportType": "月報",
-        "reportName": "2023年08月執行率統計表",
+        "reportType": "年報",
+        "reportName": "2023年年報",
     }, {
         "index": "2",
         "reportType": "月報",
-        "reportName": "2023年08月結算價金估算表",
+        "reportName": "2023年01月月報",
     }, {
         "index": "3",
         "reportType": "月報",
-        "reportName": "2023年07月結算價金估算表",
+        "reportName": "2023年02月月報",
     }, {
         "index": "4",
         "reportType": "月報",
-        "reportName": "2023年07月結算價金估算表",
+        "reportName": "2023年03月月報",
     }, {
         "index": "5",
-        "reportType": "季報",
-        "reportName": "2023年第2季季度報表",
+        "reportType": "月報",
+        "reportName": "2023年04月月報",
     }, {
         "index": "6",
         "reportType": "月報",
-        "reportName": "2023年06月結算價金估算表",
+        "reportName": "2023年05月月報",
     }, {
         "index": "7",
         "reportType": "月報",
-        "reportName": "2023年06月結算價金估算表",
+        "reportName": "2023年06月月報",
     }, {
         "index": "8",
         "reportType": "月報",
-        "reportName": "2023年05月結算價金估算表",
+        "reportName": "2023年07月月報",
     }, {
         "index": "9",
         "reportType": "月報",
-        "reportName": "2023年05月結算價金估算表",
+        "reportName": "2023年08月月報",
     }, {
         "index": "10",
         "reportType": "月報",
-        "reportName": "2023年04月結算價金估算表",
+        "reportName": "2023年09月月報",
     }, {
         "index": "11",
         "reportType": "月報",
-        "reportName": "2023年04月結算價金估算表",
+        "reportName": "2023年10月月報",
     }, {
         "index": "12",
-        "reportType": "季報",
-        "reportName": "2023年第1季季度報表",
+        "reportType": "月報",
+        "reportName": "2023年11月月報",
     }, {
         "index": "13",
         "reportType": "月報",
-        "reportName": "2023年03月結算價金估算表",
-    }, {
-        "index": "14",
-        "reportType": "月報",
-        "reportName": "2023年03月結算價金估算表",
-    }, {
-        "index": "15",
-        "reportType": "月報",
-        "reportName": "2023年02月結算價金估算表",
-    }, {
-        "index": "16",
-        "reportType": "月報",
-        "reportName": "2023年02月結算價金估算表",
-    }, {
-        "index": "17",
-        "reportType": "月報",
-        "reportName": "2023年01月結算價金估算表",
-    }, {
-        "index": "18",
-        "reportType": "月報",
-        "reportName": "2023年01月結算價金估算表",
-    }, {
-        "index": "19",
-        "reportType": "年報",
-        "reportName": "2022年年度報表",
-    }, {
-        "index": "20",
-        "reportType": "季報",
-        "reportName": "2022年第4季季度報表",
-    }, {
-        "index": "21",
-        "reportType": "月報",
-        "reportName": "2022年12月結算價金估算表",
-    }, {
-        "index": "22",
-        "reportType": "月報",
-        "reportName": "2022年12月結算價金估算表",
-    }, {
-        "index": "23",
-        "reportType": "月報",
-        "reportName": "2022年11月結算價金估算表",
-    }, {
-        "index": "24",
-        "reportType": "月報",
-        "reportName": "2022年11月結算價金估算表",
+        "reportName": "2023年12月月報",
     }];
 
     $('#reportTable').DataTable({
@@ -217,6 +109,87 @@ $(document).ready(function () {
     })
 
 });
+
+function generateYearOptions() { //動態生成年份下拉選單
+    var currentYear = new Date().getFullYear();
+    var selectElement = document.getElementById("yearDropdown");
+    for (var year = 2020; year <= currentYear; year++) {
+        var optionElement = document.createElement("option");
+        optionElement.classList.add("filtOpt");
+        optionElement.setAttribute("id", "yearRFO_" + year); // You can adjust this ID generation as needed
+        optionElement.setAttribute("value", year); // Set the value attribute
+        optionElement.textContent = year+"年";
+        selectElement.appendChild(optionElement);
+    }
+    selectElement.value = currentYear;//預設今年
+}
+
+// var myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+
+const filtMonthROpts = document.querySelector(".filtMonthR .filtOptions");
+const filtSeasonROpts = document.querySelector(".filtSeasonR .filtOptions");
+const filtYearROpts = document.querySelector(".filtYearR .filtOptions");
+
+/*const dDL_filtMonR = document.querySelector(".title #dDL_filtMonthR");
+dDL_filtMonR.addEventListener("click", showHide_filtMonROpts);
+function showHide_filtMonROpts() {
+    filtMonthROpts.classList.toggle("appear");
+    filtSeasonROpts.classList.remove("appear");
+    filtYearROpts.classList.remove("appear");
+}*/
+
+/*const dDL_filtSeaR = document.querySelector(".title #dDL_filtSeasonR");
+dDL_filtSeaR.addEventListener("click", showHide_filtSeaROpts);
+function showHide_filtSeaROpts() {
+    filtSeasonROpts.classList.toggle("appear");
+    filtMonthROpts.classList.remove("appear");
+    filtYearROpts.classList.remove("appear");
+}*/
+
+/*const dDL_filtYeaR = document.querySelector(".title #dDL_filtYearR");
+dDL_filtYeaR.addEventListener("click", showHide_filtYeaROpts);
+function showHide_filtYeaROpts() {
+    filtYearROpts.classList.toggle("appear");
+    filtMonthROpts.classList.remove("appear");
+    filtSeasonROpts.classList.remove("appear");
+}*/
+
+const filterMonthR = document.querySelector(".title .filtMonthR p");
+const filterSeasonR = document.querySelector(".title .filtSeasonR p");
+const filterYearR = document.querySelector(".title .filtYearR p");
+
+/*document.addEventListener("click", hideFiltOptions);
+function hideFiltOptions(clickItem) {
+    if ((clickItem.target.id !== "dDL_filtMonthR") && (clickItem.target.id !== "dDL_filtSeasonR") && (clickItem.target.id !== "dDL_filtYearR")) {
+        if (clickItem.target.id === "filtNone") {
+            filterMonthR.textContent = "月報";
+            filterSeasonR.textContent = "季報";
+            filterYearR.textContent = "年報";
+        } else if ((clickItem.target.id === "monthRFO_01") || (clickItem.target.id === "monthRFO_02") || (clickItem.target.id === "monthRFO_03")) {
+            filterMonthR.textContent = clickItem.target.textContent;
+            filterSeasonR.textContent = "季報";
+            filterYearR.textContent = "年報";
+        } else if ((clickItem.target.id === "seasonRFO_01") || (clickItem.target.id === "seasonRFO_02") || (clickItem.target.id === "seasonRFO_03")) {
+            filterSeasonR.textContent = clickItem.target.textContent;
+            filterMonthR.textContent = "月報";
+            filterYearR.textContent = "年報";
+        } else if ((clickItem.target.id === "yearRFO_01") || (clickItem.target.id === "yearRFO_02")) {
+            filterYearR.textContent = clickItem.target.textContent;
+            filterMonthR.textContent = "月報";
+            filterSeasonR.textContent = "季報";
+        }
+
+        filtMonthROpts.classList.remove("appear");
+        filtSeasonROpts.classList.remove("appear");
+        filtYearROpts.classList.remove("appear");
+    }
+}*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 function downloadExcel() {
     //看報表是否已存在
