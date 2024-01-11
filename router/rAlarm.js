79fdbc2258f631ce92b3b5f3a9089ab4dd24f363
HugoforEMS <starenergyems@gmail.com>
2024-01-11 15:38:27 +0800

    peter add&del: first try implem

--- router/rAlarm.js
+++ router/rAlarm.js
@@ -1,3 +1,5 @@
+const port = 3001;
+
 // testforalarm.js
 const express = require("express");
 const path = require("path");
@@ -5,11 +7,14 @@ const nano = require("nano")("http://admin:ems45877096@192.168.8.101:5984");
 //const { nano } = require("./app");
 // const other_rf01 = "other_rf01";
 // const nanoDb = nano.use(other_rf01); // 請注意這裡使用 nano.use() 來設定數據庫
-const { scaleProcess } = require("./function");
 const methodOverride = require("method-override");
 const router = express.Router();
 const app = express();
 const cors = require("cors");
+const { 
+  LC_error_result_gen, 
+  LC_error_table, } 
+  = require("./alarmFunctions");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
@@ -21,20 +26,34 @@ app.use(cors());
 app.use(cors());
 
 //************************************************************* */
-const lc1_rf10 = "lc1_rf10";
-const lc1nanoDb = nano.use(lc1_rf10);
-const lc2_rf10 = "lc2_rf10";
-const lc2nanoDb = nano.use(lc2_rf10);
-const lc3_rf10 = "lc3_rf10";
-const lc3nanoDb = nano.use(lc3_rf10);
-const lc4_rf10 = "lc4_rf10";
-const lc4nanoDb = nano.use(lc4_rf10);
-const dc_rf10 = "dc_rf10";
-const dcnanoDb = nano.use(dc_rf10);
-const other_rf01 = "other_rf01";
-const otherrf01nanoDb = nano.use(other_rf01);
-const other_rf10 = "other_rf10";
-const otherrf10nanoDb = nano.use(other_rf10);
+const lc1nanoDb = nano.use("lc1_rf10");
+const lc2nanoDb = nano.use("lc2_rf10");
+const lc3nanoDb = nano.use("lc3_rf10");
+const lc4nanoDb = nano.use("lc4_rf10");
+const dcnanoDb = nano.use("dc_rf10");
+const otherrf01nanoDb = nano.use("other_rf01");
+const otherrf10nanoDb = nano.use("other_rf10");
+
+const indexDef = {
+  index: { fields: ["time"] },
+  name: "time_index",
+};
+
+lc1nanoDb.createIndex(indexDef);
+lc2nanoDb.createIndex(indexDef);
+lc3nanoDb.createIndex(indexDef);
+lc4nanoDb.createIndex(indexDef);
+dcnanoDb.createIndex(indexDef);
+otherrf01nanoDb.createIndex(indexDef);
+otherrf10nanoDb.createIndex(indexDef);
+
+const mangoQuery = {
+  selector: {
+    time: { $exists: true },
+  },
+  sort: [{ time: "desc" }],
+  limit: 1,
+};
 //************************************************************* */
 
 //set
@@ -47,29 +66,9 @@ app.use(methodOverride("_method"));
 app.use("/public", express.static(path.join(__dirname, "../public")));
 
 //告警紀錄
-router.get("/alarm", (req, res) => {
+app.get("/alarm", (req, res) => {
   // num與fun
   try {
-    const indexDef = {
-      index: { fields: ["time"] },
-      name: "time_index",
-    };
-    lc1nanoDb.createIndex(indexDef);
-    lc2nanoDb.createIndex(indexDef);
-    lc3nanoDb.createIndex(indexDef);
-    lc4nanoDb.createIndex(indexDef);
-    dcnanoDb.createIndex(indexDef);
-    otherrf01nanoDb.createIndex(indexDef);
-    otherrf10nanoDb.createIndex(indexDef);
-
-    const mangoQuery = {
-      selector: {
-        time: { $exists: true },
-      },
-      sort: [{ time: "desc" }],
-      limit: 1,
-    };
-
     //目前查詢且輸出的是lc1nanoD內的資料
     lc1nanoDb.find(mangoQuery, async (err, body) => {
       if (err) {
@@ -77,29 +76,19 @@ router.get("/alarm", (req, res) => {
         res.status(500).send("Internal Server Error");
         return;
       }
+      // console.dir(body)
+      // const doc = body.docs[0]; // 取得數據的第一個元素
+      for (const item of body.docs) {
+        // console.log(item);
+        console.log(LC_error_result_gen(item, LC_error_table));
+      
+      };
 
-      const Data = body.docs[0]; // 取得數據的第一個元素
-      console.log("AA----------------------------------------------AA");
-      console.log(Data);
-
-      const data = {};
-
-      // Object.entries(scaleAndPointMapping).forEach(
-      //   ([property, { scale, point }]) => {
-      //     const originalValue = Data.Freq[property];
-      //     const scaledValue = scaleProcess(originalValue, scale, point);
-      //     data[property] = scaledValue;
-      //     console.log("屬性", property);
-      //     console.log("原始數值", originalValue);
-      //     console.log("轉換後數值", scaledValue);
-      //   }
-      // );
     });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
-
   res.render("Alm_RealTime");
 });
 
@@ -130,3 +119,7 @@ router.get("/alarm/history", (req, res) => {
 });
 
 module.exports = router;
+
+app.listen(port, () => {
+  console.log(`應用程式正在監聽端口 ${port}`);
+});
