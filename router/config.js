87f78691de58d2e940e626882aa2c8a4821a3419
HugoforEMS <starenergyems@gmail.com>
2024-03-09 16:00:08 +0800

    fix : 資料庫路徑修改

--- router/config.js
+++ router/config.js
@@ -4,11 +4,18 @@ module.exports = {
   // 資料庫路徑設定
   // 辦公室用
   database: {
-    host: "couchdb",
+    host: "192.168.8.101",
     port: 5984,
     username: "admin",
     password: "ems45877096",
   },
+  //改為案場電腦用
+  // database: {
+  //   host: "couchdb",
+  //   port: 5984,
+  //   username: "admin",
+  //   password: "ems45877096",
+  // },
   // 如果您要在不同的環境中使用不同的資料庫路徑，您可以在此進行配置，例如：
   //案場
   // databaseForLoc: {
