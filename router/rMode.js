fe4bd956c73087f5126722613bd6e0ddf596a3f7
HugoforEMS <starenergyems@gmail.com>
2024-03-15 13:49:51 +0800

    HCT fix: 排程設置修改

--- router/rMode.js
+++ router/rMode.js
@@ -85,11 +85,11 @@ async function query_Syscrtl_variables() {
       SOC: scaleProcess(GCData.System[400036], 0.1, 1),
       SBSPM: scaleProcess(GCData.System[400037], 0.01, 1),
       sysMode: mapSysMode(
-        GCData.System[400078] //15
+        GCData.System[400078] //5 15
       ),
 
       P_Project: scaleProcess(GCData.System[400001], 0.01, 1),
-      P_LoadShift: scaleProcess(GCData.System[400002], 0.01, 1),
+      P_LoadShift: scaleProcess(GCData.System[400002], 0.001, 1),
       statusAllPCS: mapStatusAllPCS(
         GCData.System[400078], //bit2
         GCData.System[400079], //bit2
@@ -227,8 +227,8 @@ router.get("/mode/sysctrl/:data", async (req, res) => {
 let Schd_KeyValuePairs;
 let dateNumber = 0;
 const date_MT = {
-  0: { dicName: "Today", pUW_title: "明日排程" },
-  1: { dicName: "Tomorrow", pUW_title: "今日排程" },
+  0: { dicName: "Today", subTitle: "今日排程(2024/03/15)", buttonContext: "明日排程" },
+  1: { dicName: "Tomorrow", subTitle: "明日排程(2024/03/16)", buttonContext: "今日排程" },
 };
 
 async function query_Schd_KeyValuePairs() {
@@ -280,6 +280,7 @@ async function query_Schd_KeyValuePairs() {
       use_Freq_Cmd: mapBitStatus(Convert_UInt_to_revBitString(GCData.System[400077], 16), sysCtrl_2_MT, 1),
       freqSource: mapBitStatus(Convert_UInt_to_revBitString(GCData.System[400077], 16), sysCtrl_2_MT, 0),
       Freq_test: Scale_Data(GCData.MTE[410001], 0.01, 2),
+
       exeCmdInd: "Done",
       exeCmdStatus: "執行結束",
       sbyCmdInd: "Standby",
@@ -291,13 +292,102 @@ async function query_Schd_KeyValuePairs() {
       exeCmd_P: Scale_Data(GCData.API[400993], 1, 0),
       sbyCmd_P: Scale_Data(GCData.API[400999], 1, 0),
 
-
-
-
-
-
-
-
+      P_schd_00_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401001], 0.01, 2),
+      P_schd_00_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401002], 0.01, 2),
+      P_schd_00_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401003], 0.01, 2),
+      P_schd_00_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401004], 0.01, 2),
+      P_schd_01_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401005], 0.01, 2),
+      P_schd_01_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401006], 0.01, 2),
+      P_schd_01_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401007], 0.01, 2),
+      P_schd_01_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401008], 0.01, 2),
+      P_schd_02_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401009], 0.01, 2),
+      P_schd_02_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401010], 0.01, 2),
+      P_schd_02_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401011], 0.01, 2),
+      P_schd_02_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401012], 0.01, 2),
+      P_schd_03_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401013], 0.01, 2),
+      P_schd_03_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401014], 0.01, 2),
+      P_schd_03_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401015], 0.01, 2),
+      P_schd_03_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401016], 0.01, 2),
+      P_schd_04_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401017], 0.01, 2),
+      P_schd_04_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401018], 0.01, 2),
+      P_schd_04_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401019], 0.01, 2),
+      P_schd_04_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401020], 0.01, 2),
+      P_schd_05_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401021], 0.01, 2),
+      P_schd_05_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401022], 0.01, 2),
+      P_schd_05_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401023], 0.01, 2),
+      P_schd_05_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401024], 0.01, 2),
+      P_schd_06_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401025], 0.01, 2),
+      P_schd_06_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401026], 0.01, 2),
+      P_schd_06_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401027], 0.01, 2),
+      P_schd_06_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401028], 0.01, 2),
+      P_schd_07_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401029], 0.01, 2),
+      P_schd_07_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401030], 0.01, 2),
+      P_schd_07_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401031], 0.01, 2),
+      P_schd_07_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401032], 0.01, 2),
+      P_schd_08_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401033], 0.01, 2),
+      P_schd_08_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401034], 0.01, 2),
+      P_schd_08_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401035], 0.01, 2),
+      P_schd_08_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401036], 0.01, 2),
+      P_schd_09_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401037], 0.01, 2),
+      P_schd_09_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401038], 0.01, 2),
+      P_schd_09_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401039], 0.01, 2),
+      P_schd_09_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401040], 0.01, 2),
+      P_schd_10_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401041], 0.01, 2),
+      P_schd_10_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401042], 0.01, 2),
+      P_schd_10_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401043], 0.01, 2),
+      P_schd_10_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401044], 0.01, 2),
+      P_schd_11_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401045], 0.01, 2),
+      P_schd_11_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401046], 0.01, 2),
+      P_schd_11_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401047], 0.01, 2),
+      P_schd_11_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401048], 0.01, 2),
+      P_schd_12_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401049], 0.01, 2),
+      P_schd_12_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401050], 0.01, 2),
+      P_schd_12_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401051], 0.01, 2),
+      P_schd_12_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401052], 0.01, 2),
+      P_schd_13_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401053], 0.01, 2),
+      P_schd_13_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401054], 0.01, 2),
+      P_schd_13_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401055], 0.01, 2),
+      P_schd_13_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401056], 0.01, 2),
+      P_schd_14_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401057], 0.01, 2),
+      P_schd_14_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401058], 0.01, 2),
+      P_schd_14_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401059], 0.01, 2),
+      P_schd_14_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401060], 0.01, 2),
+      P_schd_15_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401061], 0.01, 2),
+      P_schd_15_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401062], 0.01, 2),
+      P_schd_15_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401063], 0.01, 2),
+      P_schd_15_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401064], 0.01, 2),
+      P_schd_16_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401065], 0.01, 2),
+      P_schd_16_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401066], 0.01, 2),
+      P_schd_16_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401067], 0.01, 2),
+      P_schd_16_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401068], 0.01, 2),
+      P_schd_17_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401069], 0.01, 2),
+      P_schd_17_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401070], 0.01, 2),
+      P_schd_17_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401071], 0.01, 2),
+      P_schd_17_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401072], 0.01, 2),
+      P_schd_18_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401073], 0.01, 2),
+      P_schd_18_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401074], 0.01, 2),
+      P_schd_18_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401075], 0.01, 2),
+      P_schd_18_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401076], 0.01, 2),
+      P_schd_19_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401077], 0.01, 2),
+      P_schd_19_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401078], 0.01, 2),
+      P_schd_19_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401079], 0.01, 2),
+      P_schd_19_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401080], 0.01, 2),
+      P_schd_20_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401081], 0.01, 2),
+      P_schd_20_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401082], 0.01, 2),
+      P_schd_20_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401083], 0.01, 2),
+      P_schd_20_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401084], 0.01, 2),
+      P_schd_21_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401085], 0.01, 2),
+      P_schd_21_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401086], 0.01, 2),
+      P_schd_21_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401087], 0.01, 2),
+      P_schd_21_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401088], 0.01, 2),
+      P_schd_22_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401089], 0.01, 2),
+      P_schd_22_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401090], 0.01, 2),
+      P_schd_22_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401091], 0.01, 2),
+      P_schd_22_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401092], 0.01, 2),
+      P_schd_23_0: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401093], 0.01, 2),
+      P_schd_23_1: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401094], 0.01, 2),
+      P_schd_23_2: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401095], 0.01, 2),
+      P_schd_23_3: Scale_Data(GCData.Schedule[date_MT[dateNumber].dicName][401096], 0.01, 2),
 
 
     };
@@ -306,7 +396,6 @@ async function query_Schd_KeyValuePairs() {
     return null;
   }
 }
-
 
 router.get("/mode/schedule", async (req, res) => {
   try {
@@ -328,6 +417,34 @@ router.get("/mode/schedule/:data", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/change_dateNumber", async (req, res) => {
+  try {
+    console.log("接收到前端請求");
+
+    if (dateNumber === 0) {
+      dateNumber = 1;
+    } else {
+      dateNumber = 0;
+    }
+
+    console.log(dateNumber);
+
+    await query_Schd_KeyValuePairs();
+
+    const response = {
+      dateNumber: dateNumber,
+      scheduleDate: date_MT[dateNumber].subTitle,
+      anotherDay: date_MT[dateNumber].buttonContext,
+      KVPairs: Schd_KeyValuePairs,
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
   }
 });
 
